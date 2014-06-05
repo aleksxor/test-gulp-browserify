c754a2d4ee969f81a0cc737ba93d8b82c68d5573
Aleksey Khoroshev <aleksxor@gmail.com>
2014-06-05 12:11:04 +0700

    add 'clean' task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 
+var clean = require('gulp-clean');
 var browserify = require('gulp-browserify');
 var livereload = require('gulp-livereload');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var lr = require('tiny-lr');
 var server = lr();
+
+gulp.task('clean', function() {
+    gulp.src('build/**', {read: false})
+        .pipe(clean());
+});
 
 gulp.task('scripts', function() {
     gulp.src('src/first.coffee', {read: false})
@@ -49,7 +55,9 @@ gulp.task('server', function(next) {
     server.use(connect.static('build')).listen(process.env.PORT || 8000, next);
 });
 
-gulp.task('default', ['html', 'sass', 'scripts', 'lr-server', 'server'], function() {
+gulp.task('build', ['clean', 'html', 'sass', 'scripts']);
+
+gulp.task('default', ['build', 'lr-server', 'server'], function() {
     gulp.watch('src/**/*.html', {maxListeners: 999}, ['html']);
     gulp.watch('src/**/*.coffee', {maxListeners: 999}, ['scripts']);
     gulp.watch('src/**/*.scss', {maxListeners: 999}, ['sass']);
